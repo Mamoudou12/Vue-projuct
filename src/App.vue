976f56cda03652b6f6e58d0ccb222cf646dbdf4c
Mamoudou12 <bmamoudou463@gmail.com>
2024-08-06 14:56:40 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<Hello />
+
+ 
+
+
+<br>
+<br>
+<input type="text" v-model="name">
+<input type="text" v-model="age">
+
+</template>
+
+<script setup>
+  import Hello from  './components/Hello.vue'
+  import { watch, ref } from 'vue';
+
+  
+  const name = ref( 'Mamoudou' )
+  const age = ref( '26' )
+
+  watch(name, (newValue, oldValue) => {
+    console.log(newValue, oldValue);
+    
+  })
+</script>
+
+
+
+<style scoped>
+
+  .bold {
+    font-weight: bolder;
+  }
+
+  .text-red {
+    color: indianred;
+  }
+
+  .text-green {
+    color: green;
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>hELLO</h1> 
+  
+  {{ name }}
+
+
+
+  <input type="text" v-model="name" @input="handleUpdate">
+</template>
+
+
+<script setup>
+    import { ref } from 'vue';
+
+
+    const emit  = difineEmits(['updateName'])
+
+
+
+    const handleUpdate = (event) => {
+        console.log(event.target.value);
+        
+    }
+
+
+    const name = ref('')
+</script>
+
+
+
